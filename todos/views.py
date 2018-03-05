556a1e5f1d3d76271595459349fa8b62cee715f2
Ankh7016 <zxwang7016@gmail.com>
2018-03-05 15:35:34 -0500

    More pic
    More change added

--- todos/views.py
+++ todos/views.py
@@ -7,9 +7,19 @@ from django.shortcuts import render, render_to_response
 from django.views import generic
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.urls import reverse
 from django.utils import timezone
-from .models import TodoItem
+from .models import TodoItem, Market
+from django.utils.safestring import mark_safe
+
+
+class IndexView(ListView):
+    model = TodoItem
+
+def index(request):
+    response = render(request,'todos/index.html')
+    return response
 
 
 class IndexView(ListView):
@@ -23,6 +33,10 @@ def index(request):
 class MarketView(ListView):
     model = TodoItem
     template_name = 'markets.html'
+def market(request):
+    pageHtml = "hello world"
+    ret = {"pageHtml": pageHtml}
+    return render(request, "markets.html", ret)
 
 
 class CoinView(ListView):
